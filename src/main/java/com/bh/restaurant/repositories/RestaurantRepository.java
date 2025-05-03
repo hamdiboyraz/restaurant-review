7b157ceb1c14f9f575306b7a47a705e8a418c699
hamdiboyraz <hamdiboyraz19@gmail.com>
2025-05-03 20:15:26 +0300

    Add restaurant repository

---
+++ src/main/java/com/bh/restaurant/repositories/RestaurantRepository.java
@@ -0,0 +1,10 @@
+package com.bh.restaurant.repositories;
+
+import com.bh.restaurant.domain.entities.Restaurant;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class RestaurantRepository extends ElasticsearchRepository<Restaurant, String> {
+    // TODO: Custom queries
+}
