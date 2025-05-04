4f0983291806e639acabfa384ccd4118d7219610
hamdiboyraz <hamdiboyraz19@gmail.com>
2025-05-04 11:55:53 +0300

    Fix

--- src/main/java/com/bh/restaurant/repositories/RestaurantRepository.java
+++ src/main/java/com/bh/restaurant/repositories/RestaurantRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.elasticsearch.repository.ElasticsearchRepository
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class RestaurantRepository extends ElasticsearchRepository<Restaurant, String> {
+public interface RestaurantRepository extends ElasticsearchRepository<Restaurant, String> {
     // TODO: Custom queries
 }
