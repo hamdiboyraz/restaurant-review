d44658b8146b170f5f82be97e2eba52c2cd63117
hamdiboyraz <hamdiboyraz19@gmail.com>
2025-05-04 11:55:48 +0300

    Add PhotoController and uploadPhoto endpoint

---
+++ src/main/java/com/bh/restaurant/controllers/PhotoController.java
@@ -0,0 +1,26 @@
+package com.bh.restaurant.controllers;
+
+import com.bh.restaurant.domain.dtos.PhotoDto;
+import com.bh.restaurant.domain.entities.Photo;
+import com.bh.restaurant.mappers.PhotoMapper;
+import com.bh.restaurant.services.PhotoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/photos")
+public class PhotoController {
+    private final PhotoService photoService;
+    private final PhotoMapper photoMapper;
+
+    @PostMapping
+    public PhotoDto uploadPhoto(@RequestParam("file") MultipartFile file) {
+        Photo savedPhoto = photoService.uploadPhoto(file);
+        return photoMapper.toDto(savedPhoto);
+    }
+}
