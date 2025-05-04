5e5b9b545e4507bf3af33fca3c1136706f5b409c
hamdiboyraz <hamdiboyraz19@gmail.com>
2025-05-04 23:32:15 +0300

    Add get photo endpoint

--- src/main/java/com/bh/restaurant/config/SecurityConfig.java
+++ src/main/java/com/bh/restaurant/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.bh.restaurant.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -16,6 +17,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.GET, "/api/photos/**").permitAll() // GET istekleri için kimlik doğrulaması gerekmez
                         .anyRequest().authenticated() // Tüm istekler kimlik doğrulaması gerektirir
                 )
                 .oauth2ResourceServer(oauth2 ->
--- src/main/java/com/bh/restaurant/controllers/PhotoController.java
+++ src/main/java/com/bh/restaurant/controllers/PhotoController.java
@@ -5,10 +5,12 @@ import com.bh.restaurant.domain.entities.Photo;
 import com.bh.restaurant.mappers.PhotoMapper;
 import com.bh.restaurant.services.PhotoService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.MediaTypeFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
@@ -22,5 +24,17 @@ public class PhotoController {
     public PhotoDto uploadPhoto(@RequestParam("file") MultipartFile file) {
         Photo savedPhoto = photoService.uploadPhoto(file);
         return photoMapper.toDto(savedPhoto);
+    }
+
+    @GetMapping("/{id:.+}")
+    public ResponseEntity<Resource> getPhoto(@PathVariable String id) {
+        return photoService.getPhotoAsResource(id).map(photo ->
+                ResponseEntity.ok()
+                        .contentType(MediaTypeFactory
+                                .getMediaType(photo) // Detect the media type (e.g., image/jpeg, image/png)
+                                .orElse(MediaType.APPLICATION_OCTET_STREAM)) // Fallback to application/octet-stream if not found
+                        .header(HttpHeaders.CONTENT_DISPOSITION, "inline")
+                        .body(photo)
+        ).orElse(ResponseEntity.notFound().build());
     }
 }
